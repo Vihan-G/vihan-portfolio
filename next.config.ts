dc1aea4a70b7bf61b9d79e410f65a6d3f172c11a
Vihan G <vihangoenka@vihans-Laptop.local>
2026-04-17 23:24:07 -0700

    added redirects

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/resume",
+        destination: "https://drive.google.com/file/d/1yBc1fI8SnzDVejzGZ6i8NA16Z1vuvlB7/view?usp=sharing",
+        permanent: false,
+      },
+      {
+        source: "/random",
+        destination: "https://example.com",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
